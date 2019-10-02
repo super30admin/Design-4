ebcbcaa49e547a86305b3b7782f0ed004c6e9d18
Pooja Ramaswamy <poojaramaswamy94@gmail.com>
2019-10-01 20:25:21 -0700

    Completed Design 4 PR

--- peekingIterator.java
+++ peekingIterator.java
@@ -1,30 +1,33 @@
 //Time complexity: O(1)
-//Space complexity: O(N)
+//Space complexity: O(1)
 //Accepted on leetcode
-class PeekingIterator implements Iterator<Integer> {
-    Queue<Integer> list = new LinkedList<>();
-	public PeekingIterator(Iterator<Integer> iterator) {
-	    // initialize any member here.
-	    while(iterator.hasNext())
-        {
-            list.add(iterator.next());
-        }
-	}
+class PeekingIterator implements Iterator<Integer> {  
+    private Integer next = null;
+    private Iterator<Integer> iter;
 
-    // Returns the next element in the iteration without advancing the iterator.
-	public Integer peek() {
-        return list.peek();
-	}
+    public PeekingIterator(Iterator<Integer> iterator) {
+        // initialize any member here.
+        iter = iterator;
+        if (iter.hasNext())
+            next = iter.next();
+    }
+    
+    // Returns the next element in the iteration without advancing the iterator. 
+    public Integer peek() {
+        return next; 
+    }
 
-	// hasNext() and next() should behave the same as in the Iterator interface.
-	// Override them if needed.
-	@Override
-	public Integer next() {
-	    return list.poll();
-	}
+    // hasNext() and next() should behave the same as in the Iterator interface.
+    // Override them if needed.
+    @Override
+    public Integer next() {
+        Integer res = next;
+        next = iter.hasNext() ? iter.next() : null;
+        return res; 
+    }
 
-	@Override
-	public boolean hasNext() {
-	    return !list.isEmpty();
-	}
+    @Override
+    public boolean hasNext() {
+        return next != null;
+    }
 }
