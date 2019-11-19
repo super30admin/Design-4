2491091196fe45e2524d1dca9d25858395ce8cc8
Ajay Chauhan <chauhanajayvikram1992@gmail.com>
2019-11-19 05:34:13 -0800

    Completed Twitter.

---
+++ src/demo/SkipIterator.java
@@ -0,0 +1,32 @@
+package demo;
+
+import java.util.Iterator;
+
+class SkipIterator implements Iterator<Integer> {
+
+	public SkipIterator(Iterator<Integer> it) {
+	}
+
+	public boolean hasNext() {
+		return false;
+	}
+
+	public Integer next() {
+		return null;
+	}
+
+	
+	public void advance() {
+		
+	}
+	
+	public void skip() {
+		
+	}
+	/**
+	* The input parameter is an int, indicating that the next element equals 'val' needs to be skipped.
+	* This method can be called multiple times in a row. skip(5), skip(5) means that the next two 5s should be skipped.
+	*/ 
+	public void skip(int val) {
+	}
+}
