320f990c8e43bd66a75c718ee3033847e326009b
Lohith <a.lohith@yahoo.in>
2020-02-11 19:04:11 -0600

    Design 4

---
+++ Skip_Iterator.java
@@ -0,0 +1,55 @@
+// Time Complexity : O(n) 
+// Space Complexity : O(n)
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : No
+
+class SkipIterator implements Iterator<Integer> {
+
+	HashMap<Integer, Integer> skipMap;
+	Iterator<Integer> it;
+	Integer nextel;
+
+	public SkipIterator(Iterator<Integer> it) {
+		skipMap = new HashMap<>();
+		this.it = it;
+		advance();
+	}
+
+	private void advance() {
+		nextel = null;
+		while (nextel == null && it.hasNext()) {
+			Integer el = it.next();
+			if (!skipMap.containsKey(el)) {
+				nextel = el;
+			} else {
+				skipMap.put(el, skipMap.get(el) - 1);
+				skipMap.remove(el, 0);
+			}
+		}
+	}
+
+	public boolean hasNext() {
+		return nextel != null;
+	}
+
+	public Integer next() {
+		int temp = nextel;
+		advance();
+		return temp;
+	}
+
+
+
+	/**
+	* The input parameter is an int, indicating that the next element equals 'val' needs to be skipped.
+	* This method can be called multiple times in a row. skip(5), skip(5) means that the next two 5s should be skipped.
+	*/ 
+
+	public void skip(int val) {
+		if (val == nextel) {
+			advance();
+		} else {
+			skipMap.put(val, skipMap.getOrDefault(val, 0) + 1);
+		}
+	}
+}
