c83b26e50bdcf0c5445d536961b9a68b866ac6ad
laxmikantbpandhare <laxmikantpandhare@gmail.com>
2020-10-04 13:32:06 -0700

    modief

--- skip_Iterator.java
+++ skip_Iterator.java
@@ -9,25 +9,40 @@
         # Optimized approach: 
                               
             # 1. 
-                    A) 
+                    A) Used HashMap to keep track of skipped elements.
+                    B) Before returning any element, checked if it is part of hashmap or not if not then return and increase 
+                       the counter.
+                    C) If it part of it then remove or decrease the counter from hashmap of that element and increase the counter to 
+                       return the next element from the arary or iterator.
     */  
 import java.util.*;
 class SkipIterator implements Iterator<Integer> {
     HashMap<Integer,Integer> hm;
-	public SkipIterator(Iterator<Integer> it) {
+    int counter;
+    int arr[];
+    //for time being I assed array instead of iterator
+	public SkipIterator(int arr[]) {
         hm = new HashMap<>();
-        
+        counter = 0;
+        this.arr = arr;
     }
 
-// ADDED FILES
-
-
 	public boolean hasNext() {
-
+        return (counter <= arr.length-1);
 	}
 
 	public Integer next() {
-        
+        if(hm.containsKey(arr[counter])){
+            if(hm.get(arr[counter])>1){
+                hm.put(arr[counter],hm.get(arr[counter])-1);
+            }else{
+                hm.remove(arr[counter]);
+            }
+            counter++;
+        }
+        int k = arr[counter];
+        counter++;
+        return k;
 	}
 
 	/**
@@ -35,26 +50,46 @@ class SkipIterator implements Iterator<Integer> {
 	* This method can be called multiple times in a row. skip(5), skip(5) means that the next two 5s should be skipped.
 	*/ 
 	public void skip(int val) {
+
         if(hm.containsKey(val)){
             hm.put(val,hm.get(val)+1);
         }else
             hm.put(val,1);
+        
 	}
 
     public static void main(String args[]){
-        SkipIterator itr = new SkipIterator(new Iterator(2, 3, 5, 6, 5, 7, 5, -1, 5, 10));
-        itr.hasNext(); // true
-        itr.next(); // returns 2
+        int arr[] = new int[]{2, 3, 5, 6, 5, 7, 5, -1, 5, 10};
+        SkipIterator itr = new SkipIterator(arr);
+        System.out.println(itr.hasNext()); // true
+        System.out.println(itr.next()); // returns 2
         itr.skip(5);
-        itr.next(); // returns 3
-        itr.next(); // returns 6 because 5 should be skipped
-        itr.next(); // returns 5
+        System.out.println(itr.next()); // returns 3
+        System.out.println(itr.next()); // returns 6 because 5 should be skipped
+        System.out.println(itr.next()); // returns 5
         itr.skip(5);
         itr.skip(5);
-        itr.next(); // returns 7
-        itr.next(); // returns -1
-        itr.next(); // returns 10
-        itr.hasNext(); // false
-        itr.next(); // error
+        System.out.println(itr.next()); // returns 7
+        System.out.println(itr.next()); // returns -1
+        System.out.println(itr.next()); // returns 10
+        System.out.println(itr.hasNext()); // false
+        System.out.println(itr.next()); // error
     }
 }
+
+// o/p came as per expectation
+// (base) Laxmikants-MacBook-Pro:Design-4 laxmikantbhaskarpandhare$ java SkipIterator
+// true
+// 2
+// 3
+// 6
+// 5
+// 7
+// -1
+// 10
+// false
+// Exception in thread "main" java.lang.ArrayIndexOutOfBoundsException: 10
+// 	at SkipIterator.next(skip_Iterator.java:31)
+// 	at SkipIterator.main(skip_Iterator.java:72)
+// (base) Laxmikants-MacBook-Pro:Design-4 laxmikantbhaskarpandhare$ 
+
