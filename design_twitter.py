edaa3829ed77d67fc46146e368814e02e1412f90
Ashutosh Personal <ashutoshfolane@gmail.com>
2020-10-27 01:04:46 -0700

    Completed Design-4

--- design_twitter.py
+++ design_twitter.py
@@ -1,8 +1,23 @@
 """
 Problem: 355. Design Twitter
 Leetcode: https://leetcode.com/problems/design-twitter/
-
+Time Complexity: O(n log n) for getNewsFeed, other O(1)
+Space Complexity: O(users * max no of tweets of a user + users * max no of followers for a user)
+Did this code successfully run on Leetcode: Yes
 """
+
+from heapq import heappush, heappop
+
+
+# Create a class Tweet to store timestamp for each tweet along with its tweetId
+class Tweet:
+    def __init__(self, tweetId, timestamp):
+        self.tweetId = tweetId
+        self.timestamp = timestamp
+
+    # Add a custom comparator function for heap/sorting
+    def __lt__(self, other):
+        return self.timestamp < other.timestamp
 
 
 class Twitter:
@@ -10,56 +25,73 @@ class Twitter:
         """
         Initialize your data structure here.
         """
-        self.users = {}
-        self.followers = {}
-        self.assign_priority = 0
+        # global timestamp to update the timestamp for each tweet and increment globally
+        self.timestamp = 0
+        # In user_tweet hashmap, key is userId and value is a list of tweetIds
+        self.user_tweet = {}
+        # In user_follower hashmap, followerId is key and followeeId is value in hashset
+        self.user_follower = {}
+        self.feed_limit = 10
+
+    def _newUser(self, userId):
+        # add userid itself into its own follower's hashset so that its post will be included in news feed
+        if userId not in self.user_follower:
+            self.user_follower[userId] = set()
+            self.user_follower[userId].add(userId)
+
+        # for new user, create an empty list of tweets
+        if userId not in self.user_tweet:
+            self.user_tweet[userId] = []
 
     def postTweet(self, userId: int, tweetId: int) -> None:
         """
         Compose a new tweet.
         """
-        self.assign_priority += 1
-        if userId in self.users:
-            self.users[userId].append((tweetId, self.assign_priority))
-        else:
-            self.users[userId] = [(tweetId, self.assign_priority)]
-
-    def get_tweet(self, userId):
-        tweets = []
-        if userId in self.users:
-            tweets += self.users[userId]
-        if userId in self.followers:
-            followees = self.followers[userId]
-            for u_id in followees:
-                if u_id in self.users:
-                    tweets += self.users[u_id]
-        tweets = sorted(tweets, key=lambda posts: posts[1], reverse=True)
-        return tweets[0:10]
+        self._newUser(userId)
+        tweet = Tweet(tweetId, self.timestamp)
+        self.user_tweet[userId].append(tweet)
+        self.timestamp += 1
 
     def getNewsFeed(self, userId: int) -> List[int]:
         """
         Retrieve the 10 most recent tweet ids in the user's news feed. Each item in the news feed must be posted by users who the user followed or by the user herself. Tweets must be ordered from most recent to least recent.
         """
-        recent_Tweets = self.get_tweet(userId)
-        return [post[0] for post in recent_Tweets]
+        self._newUser(userId)
+        tweet_list = []
+        followedUsers = self.user_follower[userId]
+        if followedUsers:
+            for followee in followedUsers:
+                if followee in self.user_tweet and len(self.user_tweet[followee]) > 0:
+                    # Using priority queue to maintain the sequence of tweets
+                    for tweet in self.user_tweet[followee]:
+                        heappush(tweet_list, tweet)
+                        if len(tweet_list) > self.feed_limit:
+                            heappop(tweet_list)
+        output = []
+        while len(tweet_list) > 0:
+            output.append(heappop(tweet_list).tweetId)
+
+        return output[::-1]
 
     def follow(self, followerId: int, followeeId: int) -> None:
         """
         Follower follows a followee. If the operation is invalid, it should be a no-op.
         """
-        if followerId != followeeId:
-            if followerId in self.followers:
-                self.followers[followerId].add(followeeId)
-            else:
-                self.followers[followerId] = {followeeId}
+        self._newUser(followerId)
+        self._newUser(followeeId)
+        self.user_follower[followerId].add(followeeId)
 
     def unfollow(self, followerId: int, followeeId: int) -> None:
         """
         Follower unfollows a followee. If the operation is invalid, it should be a no-op.
         """
-        if followerId in self.followers:
-            if followeeId in self.followers[followerId]:
-                self.followers[followerId].remove(followeeId)
+        self._newUser(followerId)
+        self._newUser(followeeId)
+        # check if key exists i.e follower is present and follower is not himself/herself
+        if followerId in self.user_follower and followerId != followeeId:
+            # check the values of particular followerId
+            if followeeId in self.user_follower[followerId]:
+                self.user_follower[followerId].remove(followeeId)
 
 # Your Twitter object will be instantiated and called as such:
 # obj = Twitter()
--- skip_iterator.py
+++ skip_iterator.py
@@ -3,6 +3,71 @@ Problem: Design a Skip Iterator that supports a method skip(int val).
     - When it is called the next element equals val in iterator sequence should be skipped.
 Leetcode: https://leetcode.com/discuss/interview-question/341818/Google-or-Onsite-or-Skip-Iterator
 Approach:
-Time Complexity:
-Space Complexity:
+    - Use functionality of inbuilt iterator
+    - Keep track of elements and count in hashmap for elements to be skipped
+    - Update cursor for next value
+Time Complexity: O(n) for skipping elements in hashmap in worst case
+Space Complexity: O(n) for hashmap of elements to be skipped
+Did this code successfully run on Leetcode: Yes
 """
+
+
+class SkipIterator:
+    def __init__(self, iterator):
+        self.iterator = iterator
+        self.skipMap = {}
+        self.cursor = None
+        self.__seekCursor()
+
+    def hasNext(self) -> bool:
+        return self.cursor != None
+
+    def next(self) -> int:
+        element = self.cursor
+        self.__seekCursor()
+        return element
+
+    def skip(self, val: int) -> None:
+        # no need to update count in hashmap if current value is skipped
+        if self.cursor == val:
+            self.__seekCursor()
+        else:
+            if val in self.skipMap:
+                self.skipMap[val] += 1
+            else:
+                self.skipMap[val] = 1
+
+    def __seekCursor(self) -> None:
+        self.cursor = None
+        nextValue = next(self.iterator, None)
+        # iterate till either cursor does not contain a valid value or there is no next value to be fetched
+        while self.cursor is None and nextValue is not None:
+            element = nextValue
+
+            # if element is present and not in skipped map, cursor can be initialized
+            if element not in self.skipMap:
+                self.cursor = element
+                break
+            # decrement the count from skip map for the current element as it has been skipped while iterating
+            else:
+                self.skipMap[element] -= 1
+                if self.skipMap[element] == 0:
+                    del self.skipMap[element]
+
+            nextValue = next(self.iterator, None)
+
+
+itr = SkipIterator(iter([2, 3, 5, 6, 5, 7, 5, -1, 5, 10]))
+print(itr.hasNext())   # true
+print(itr.next())      # returns 2
+itr.skip(5)
+print(itr.next())      # returns 3
+print(itr.next())      # returns 6 because 5 should be skipped
+print(itr.next())      # returns 5
+itr.skip(5)
+itr.skip(5)
+print(itr.next())      # returns 7
+print(itr.next())      # returns -1
+print(itr.next())      # returns 10
+print(itr.hasNext())   # returns false
+print(itr.next())      # None
