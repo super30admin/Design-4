849bd5b96ea84ae9b8d91de806b3c20afe96355b
Adhish Shrivastava <adhish70@gmail.com>
2021-11-30 12:39:17 -0500

    Problem86 done

---
+++ Problem-85 Design Twitter.py
@@ -0,0 +1,32 @@
+# 355. Design Twitter
+# https://leetcode.com/problems/design-twitter/
+
+# Logic:
+
+# Time Complexity:
+# Space Complexity: 
+
+class Twitter:
+    def __init__(self):
+        pass
+
+    def postTweet(self, userId: int, tweetId: int) -> None:
+        pass
+
+    def getNewsFeed(self, userId):
+        pass
+
+    def follow(self, followerId: int, followeeId: int) -> None:
+        pass
+
+    def unfollow(self, followerId: int, followeeId: int) -> None:
+        pass
+        
+
+
+# Your Twitter object will be instantiated and called as such:
+# obj = Twitter()
+# obj.postTweet(userId,tweetId)
+# param_2 = obj.getNewsFeed(userId)
+# obj.follow(followerId,followeeId)
+# obj.unfollow(followerId,followeeId)
