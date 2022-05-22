90bb2d1ce1f3a9a18e586cd06b604a135c9762a1
r-ashwini <34915478+r-ashwini@users.noreply.github.com>
2022-05-22 12:46:38 -0700

    Create designtwitter.java

---
+++ designtwitter.java
@@ -0,0 +1,13 @@
+/*
+Two approached Strategical -> SYStem level approach 
+Tactical approach -> grass root level approach 
+
+First Question to ask interviewer : could you elaborate on the features/ requirements when designing twitter/ 
+Ask interviewer where should I start : at backend or at database 
+// At database level : Object Oriented Design level : if interview is asking to start at database its object oriented design 
+// 
+
+
+
+
+*/
