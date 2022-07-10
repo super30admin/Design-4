01928e460b51a06fc80ba8fe65eecaaebdce5399
plokaiah <plokaiah@asu.edu>
2022-07-10 10:22:24 -0700

    Completed design 4

---
+++ DesignTwitter.py
@@ -0,0 +1,81 @@
+import heapq as heap
+class Tweet(object):
+    def __init__(self, tweetId, createdAt):
+        self.tweetId=tweetId
+        self.createdAt=createdAt
+class Twitter(object):
+
+    def __init__(self):
+        #We need User:Tweets mapping and User: user mapping (list of following)
+        self.tweets=dict()
+        self.following=dict()
+        self.timestamp=0
+    def postTweet(self, userId, tweetId):
+        """
+        :type userId: int
+        :type tweetId: int
+        :rtype: None
+        """
+        #When the user is posting the tweet, make the user follow itself 
+        self.follow(userId,userId)
+        if userId not in self.tweets:
+            self.tweets[userId]=[]
+        self.timestamp+=1
+        self.tweets[userId].append(Tweet(tweetId, self.timestamp))
+
+    def getNewsFeed(self, userId):
+        """
+        :type userId: int
+        :rtype: List[int]
+        """
+        #Get the set of followings for the userid
+        follows=self.following.get(userId)
+        #Create an empty list for priority queue for tweets
+        pq=[]
+        
+        #Custom less than comparator for tweets
+        moreThan = lambda x, y: x.createdAt<y.createdAt
+        Tweet.__lt__ = moreThan
+        
+        if follows is not None:
+            for user in follows:
+                usertweets=self.tweets.get(user)
+                if usertweets is not None:
+                    for tweet in usertweets:
+                        #print(tweet.createdAt)
+                        heap.heappush(pq,tweet)
+                        if len(pq)>10: 
+                            heap.heappop(pq)      
+        result=[]
+        #print(pq)
+        while len(pq)!=0:
+            result.insert(0,heap.heappop(pq).tweetId)
+        return result
+
+    def follow(self, followerId, followeeId):
+        """
+        :type followerId: int
+        :type followeeId: int
+        :rtype: None
+        """
+        if followerId not in self.following:
+            self.following[followerId]=set()
+        self.following[followerId].add(followeeId)
+
+    def unfollow(self, followerId, followeeId):
+        """
+        :type followerId: int
+        :type followeeId: int
+        :rtype: None
+        """
+        if followerId in self.following and followerId!= followeeId:
+            if followeeId in self.following[followerId]:
+                self.following[followerId].remove(followeeId)    
+
+
+# Your Twitter object will be instantiated and called as such:
+# obj = Twitter()
+# obj.postTweet(userId,tweetId)
+# param_2 = obj.getNewsFeed(userId)
+# obj.follow(followerId,followeeId)
+# obj.unfollow(followerId,followeeId)
