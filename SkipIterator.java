59da62660593ecae38089cbb7fd91d1d19e66f5d
jayachandravvs <42566670+jayachandravvs@users.noreply.github.com>
2022-07-13 20:51:01 -0500

    Create SkipIterator.java

---
+++ SkipIterator.java
@@ -0,0 +1,80 @@
+class SkipIterator implements Iterator<Integer> {
+
+     private final Iterator<Integer> it;
+
+     private final Map<Integer, Integer> count;
+
+     private Integer nextEl;
+
+   public SkipIterator(Iterator<Integer> it) {
+this.it = it;
+
+         this.count = new HashMap<>();
+
+         advance();
+
+     }
+
+   @Override
+
+     public boolean hasNext() {
+
+         return nextEl != null;
+
+     }
+
+   @Override
+
+     public Integer next() {
+
+         if (!hasNext()) throw new RuntimeException("empty");
+
+         Integer el = nextEl;
+
+         advance();
+
+         return el;
+
+     }
+
+   public void skip(int num) {
+
+         if (!hasNext()) throw new RuntimeException("empty");
+
+         if (nextEl == num) {
+
+             advance();
+
+         } else {
+
+             count.put(num, count.getOrDefault(num, 0) + 1);
+
+         }
+
+     }
+
+   private void advance() {
+
+         nextEl = null;
+
+         while (nextEl == null && it.hasNext()) {
+
+             Integer el = it.next();
+
+             if (!count.containsKey(el)) {
+
+                 nextEl = el;
+
+             } else {
+
+                 count.put(el, count.get(el) - 1);
+
+                 count.remove(el, 0);
+
+             }
+
+         }
+
+     }
+
+ }
