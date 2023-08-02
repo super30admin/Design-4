69bda5671e2682898c01fbd5af98c525538f84db
Bharath Barakam <bharath.barakam@ultra.me>
2023-08-01 22:40:04 -0700

    Completed Design-4

---
+++ skipIterator.java
@@ -0,0 +1,35 @@
+//Time Complexity: O(n)
+//Space Complexity: O(n)
+
+class SkipIterator {
+	private int index = 0;
+	private int[] nums = null;
+	private Map<Integer, Integer> map = null;
+	public SkipIterator(int[] nums) {
+		this.nums = nums;
+		map = new HashMap<>();
+	}
+
+	/**
+	* Returns true if the iteration has more elements.
+	*/
+	public boolean hasNext() {
+		return index < nums.length;
+	}
+
+	/**
+	* Returns the next element in the iteration.
+	*/
+	public Integer next() {
+		Integer value = nums[index++];
+		checkSkipped();
+		return value;
+	}
+	
+	private void checkSkipped() {
+		while(index < nums.length && map.containsKey(nums[index])) {
+			if(map.get(nums[index]) == 1) map.remove(nums[index]);
+			else map.put(nums[index], map.get(nums[index])-1);
+			++index;
+		}
+	}
